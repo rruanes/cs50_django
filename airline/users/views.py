734b2fc2d0bb6d3681a60b761ca1302cedf5e29f
Ronnel <rruanes@gmail.com>
2023-08-18 17:04:02 +0800

    First commit

--- airline/users/views.py
+++ airline/users/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render
 from django.urls import reverse
 from django.http import HttpResponseRedirect
 
-# Create your views here.
+
 def index(request):
     if not request.user.is_authenticated:
         return HttpResponseRedirect(reverse("login"))
